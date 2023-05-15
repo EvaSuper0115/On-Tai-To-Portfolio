a3123de286d8ccdd10123ffba5691e51b5bc28d0
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-05-15 13:31:54 +0100

    added parallex to resume

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const workAndAboutLinks = document.querySelector(
 );
 const serviceTitle = document.querySelector("#service");
 const serviceGrid = document.querySelector(".service-grid");
+const resumeLink = document.querySelector("#resume");
 const myWorkTitle = document.querySelector("#my-work-title");
+
 window.addEventListener("scroll", () => {
   let offsetY = window.scrollY;
   nameAndTitle.style.transform = `translateX(${-offsetY * 0.3}px)`;
   workAndAboutLinks.style.transform = `translateX(${offsetY * 0.3}px)`;
   serviceTitle.style.transform = `translateY(${-offsetY * 0.3}px)`;
+  serviceGrid.style.transform = `translateY(${-offsetY * 0.2}px)`;
+  resumeLink.style.transform = `translateY(calc(490px - ${offsetY}px))`;
 });
