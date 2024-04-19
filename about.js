6b1eacee07ce6eb68e113239391f50d93497b1a9
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-19 13:43:48 +0100

    make profile pic move with mouse

--- about.js
+++ about.js
@@ -4,11 +4,12 @@ window.addEventListener("scroll", () => {
   let offsetY = window.scrollY;
 });
 
-const container = document.querySelector(".container");
+//making profile move with mouse movement
+const container = document.querySelector(".about-me-page-intro-row");
 const profilePicture = document.getElementById("profile-picture");
-
+const circle = document.querySelector(".circle");
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 30;
-  console.log(xAxis);
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+  profilePicture.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
 });
