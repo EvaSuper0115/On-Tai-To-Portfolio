1da68d42cb2a0bb831a642c86dec47327f3de79a
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-19 13:06:27 +0100

    Update about.js

--- about.js
+++ about.js
@@ -3,3 +3,12 @@ const skillSetLogos = document.querySelectorAll(".skills");
 window.addEventListener("scroll", () => {
   let offsetY = window.scrollY;
 });
+
+const container = document.querySelector(".container");
+const profilePicture = document.getElementById("profile-picture");
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 30;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 30;
+  console.log(xAxis);
+});
