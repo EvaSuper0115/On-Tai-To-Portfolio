8ef834767d6777d3442011f55da52b8d84b67343
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2024-04-19 14:46:05 +0100

    Update about.js

--- about.js
+++ about.js
@@ -9,7 +9,7 @@ const container = document.querySelector(".about-me-page-intro-row");
 const profilePicture = document.getElementById("profile-picture");
 const circle = document.querySelector(".circle");
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
   profilePicture.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
 });
